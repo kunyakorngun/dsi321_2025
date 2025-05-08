555c1d48bc0c76031c50c6c33d36db572385c161
kunyakorn <gun.gun2546@gmail.com>
2025-05-08 13:50:44 +0700

    update main:Construction supplies

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from datetime import datetime
 
 # รายการคำค้นหาที่ต้องการ
 keywords = [
-    "cement industry",
+    "Construction supplies",
 ]
 
 # เวลาที่ดึงข้อมูล
 fetch_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # เปิดไฟล์ CSV เพื่อเขียนข้อมูล
-with open("cement industry_scrap.csv", mode="w", encoding="utf-8", newline="") as file:
+with open("Construction supplies_scrap.csv", mode="w", encoding="utf-8", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["Fetched Time", "Keyword", "Title", "URL"])
 
